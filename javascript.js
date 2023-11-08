98e64b5bad47dad416d98043cda3381dca8761fa
flavia sigoli <flaviasivm@gmail.com>
2023-11-08 18:04:28 -0300

    alteraçoes 2 e toggle menup1

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 //navlinks menu mobile
-const toggleButton = document.getElementsByClassName('btn-toggle')[0]
-const navbarLinks = document.getElementsByClassName('menu-links', 'menusocial')[0]
+let menu_icon_box = document.querySelector(".btn-toggle");
+let box = document.querySelector(".box");
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+
+menu_icon_box.onclick = function(){
+    menu_icon_box.classList.toggle("active");
+    box.classList.toggle("active_box");
+}
+document.onclick = function(e){
+    if (!menu_icon_box.contains(e.target) && !box.contains(e.target) ) {
+        menu_icon_box.classList.remove("active");
+        box.classList.remove("active_box");
+    }
+}
