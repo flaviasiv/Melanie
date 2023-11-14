129e883fe036116769f6873f299b21479b3a0d06
flavia sigoli <flaviasivm@gmail.com>
2023-11-14 11:41:52 -0300

    alteracoes5

--- javascript.js
+++ javascript.js
@@ -20,7 +20,6 @@ const swiperEl = document.querySelector('.mySwiper2')
 Object.assign(swiperEl, {
   slidesPerView: 1,
   spaceBetween: 0,
-  loop: true,
   pagination: {
     clickable: true,
   },
@@ -40,3 +39,46 @@ Object.assign(swiperEl, {
   },
 });
 swiperEl.initialize();
+
+// scroll behavior
+let section = document.querySelectorAll("main");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]", ".testemonycard")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 10;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+//reveal();
